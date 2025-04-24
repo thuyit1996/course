5a2d0f60fb5478a487838afec151de69909c58fd
Vu Thuy <vuthuycntt1996@gmail.com>
2025-04-24 22:45:30 +0700

    add confirm modal

--- src/components/ui/confirm-modal/index.tsx
+++ src/components/ui/confirm-modal/index.tsx
@@ -1,37 +1,37 @@
 import { useModal } from "@/hooks/useModal";
 import { Modal } from "../modal"
-
-const ConfirmModal = () => {
+import Button from "../button/Button";
+interface ConfirmModalProps {
+    isShowFooter?: boolean;
+    closeModalProp : () => void;
+    handeSaveProp:() => void
+}
+const ConfirmModal = ({ isShowFooter, closeModalProp, handeSaveProp}: ConfirmModalProps) => {
     const { isOpen, openModal, closeModal } = useModal();
     return (
         <Modal
-            isOpen={isOpen}
-            onClose={closeModal}
-            className="max-w-[600px] p-5 lg:p-8"
-        >
-            <h4 className="font-semibold text-gray-800 mb-7 text-title-sm dark:text-white/90">
-                Modal Heading
-            </h4>
-            <p className="text-sm leading-6 text-gray-500 dark:text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque euismod est quis mauris lacinia pharetra. Sed a ligula
-                ac odio condimentum aliquet a nec nulla. Aliquam bibendum ex sit
-                amet ipsum rutrum feugiat ultrices enim quam.
-            </p>
-            <p className="mt-5 text-sm leading-6 text-gray-500 dark:text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque euismod est quis mauris lacinia pharetra. Sed a ligula
-                ac odio.
-            </p>
-            <div className="flex items-center justify-end w-full gap-3 mt-8">
-                {/* <Button size="sm" variant="outline" onClick={closeModal}>
-                    Close
-                </Button>
-                <Button size="sm" onClick={handleSave}>
-                    Save Changes
-                </Button> */}
-            </div>
-        </Modal>
+        isOpen={isOpen}
+        onClose={closeModal}
+        className="max-w-[600px] p-5 lg:p-8"
+      >
+        <h4 className="font-semibold text-[#2c2c2c] mb-3 text-[24px] leading-8">
+          Have you finished your writing?
+        </h4>
+        <p className="text-base text-[#2c2c2c]">
+        Once you submit, you won’t be able to edit your response.<br/> Are you sure you want to submit?
+        </p>
+        {
+isShowFooter ?        <div className="flex items-center justify-end w-full gap-3 mt-6">
+          <Button size="md" variant="outline" onClick={closeModal}>
+            Cancel
+          </Button>
+          <Button size="md" variant="secondary" onClick={handleSave}>
+            Save Changes
+          </Button>
+        </div> : null
+        }
+
+      </Modal>
     )
 }
 export default ConfirmModal
